282ff396c90990f088e23b57a18d95d3de5567b6
Drella <drellayan@gmail.com>
2024-10-08 17:58:52 +0800

    yaml

--- scripts/data_preparation/noCrop.py
+++ scripts/data_preparation/noCrop.py
@@ -2,6 +2,7 @@ from basicsr.utils.create_lmdb import create_lmdb_for_gopro
 
 def main():
     opt = {}
+    opt['compression_level'] = 3
 
     opt['save_folder'] = './datasets/Sim/train/blur_crops'
     opt['save_folder'] = './datasets/Sim/train/sharp_crops'
